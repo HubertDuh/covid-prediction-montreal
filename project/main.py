a4309c3e69b4999f4bd591728c244dd2095086df
HubertDuh <rAkse7-hemsek-xedvyf>
2022-05-31 00:19:20 -0400

    Final commit until I get better at data science. THis final commit uses a 2nd degree polynomial function to predict how much people are going to get covid. Very rudimentary prediction as the curve doesn't follow the data

--- project/main.py
+++ project/main.py
@@ -1,12 +1,10 @@
-import lmfit
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-from scipy.integrate import odeint
-from lmfit import Parameters, minimize
-from copy import deepcopy
-from tqdm.auto import tqdm
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+
 
 # the csv file is at the following url: https://ici.radio-canada.ca/info/2020/coronavirus-covid-19-pandemie-cas-carte-maladie-symptomes-propagation/index-en.html
 
@@ -83,13 +81,45 @@ while days_deaths < len(montreal_data_matrix):
 
 montreal_data['Daily_Deaths'] = deaths_daily
 
-# Here, we will clean up our dataframe so that it is cleaner and easier to work with to do SEIRD
+id_array = []
+i=1
+for j in range(len(montreal_data_matrix)):
+   id_array.append(i)
+   i += 1
+
+montreal_data['id'] = id_array
+
+# Here, we will clean up our dataframe so that it is cleaner and easier to work with
 
 montreal_data.drop(['Hospitalizations', 'ICU', 'Tested', 'Positivity', 'Vaccinated', 'Vaccinated 2', 'Vac Distributed',
                     'Total variants', 'Variant B.1.1.7', 'Variant B.1.351', 'Variant P.1', 'Variant Nigeria',
                     'Variant unknown', 'Variant B.1.617', 'Variant B.1.1.529', 'Vaccinated 1', 'Vaccinated 3'],
                    axis=1, inplace=True)
 
+# Prepare the data
+x = np.array(montreal_data['id']).reshape(-1, 1)
+y = np.array(montreal_data['Confirmed']).reshape(-1, 1)
+plt.plot(y, '-m')
+#plt.show()
+polyFeat = PolynomialFeatures(degree=2)
+x = polyFeat.fit_transform(x)
+print(x)
 
-# Now, we build the barebone SEIR model
+# Training the data
 
+model = linear_model.LinearRegression()
+model.fit(x, y)
+accuracy = model.score(x, y)
+print(f'Accuracy:{round(accuracy*100, 3)} %')
+y0 = model.predict(x)
+
+
+
+# Prediction
+days = 30
+
+x1 = np.array(list(range(1, 811+days))).reshape(-1, 1)
+y1 = model.predict(polyFeat.fit_transform(x1))
+plt.plot(y1, '--r')
+plt.plot(y0, '--b')
+plt.show()
